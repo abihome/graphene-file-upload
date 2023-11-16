e74d8dc05eb1634e690dae44af326864a2f29304
Elastoplastique <canburak@msn.com>
2023-11-16 10:09:52 +0300

    first commit

---
+++ tests/flask_app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from graphene_file_upload.flask import FileUploadGraphQLView
+
+
+def create_app(schema, path='/graphql'):
+    app = Flask(__name__)
+    app.debug = True
+    app.add_url_rule(
+        path,
+        view_func=FileUploadGraphQLView.as_view(
+            'graphql',
+            schema=schema,
+        ),
+    )
+    return app
